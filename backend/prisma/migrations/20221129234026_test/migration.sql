6b4c9525e7f38791c1e8400feeab39cad35446fb
Nathan PERIER <periernathan2000@gmail.com>
2022-11-30 20:38:47 +0100

    Updated database schema

--- backend/prisma/migrations/20221129234026_test/migration.sql
+++ backend/prisma/migrations/20221129234026_test/migration.sql
@@ -5,10 +5,10 @@ CREATE TYPE "Visibility" AS ENUM ('PUBLIC', 'PRIVATE', 'RESTRICTED');
 CREATE TYPE "Role" AS ENUM ('USER', 'ADMIN');
 
 -- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "email" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
+CREATE TABLE "users" (
+    "id" TEXT UNIQUE NOT NULL,
+    "email" TEXT UNIQUE NOT NULL,
+    "username" TEXT UNIQUE NOT NULL,
     "password" TEXT NOT NULL,
     "salt" TEXT NOT NULL,
     "visibility" "Visibility" NOT NULL DEFAULT 'PRIVATE',
@@ -17,11 +17,8 @@ CREATE TABLE "User" (
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "users_email_key" ON "User"("email");
