156aac3f0fb8e971ab27e039ec1b3ba886026b60
Nathan PERIER <periernathan2000@gmail.com>
2022-11-30 16:58:37 +0100

    Uncommented code

--- backend/src/core/api.ts
+++ backend/src/core/api.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import router from "./router";
 import cookieParser from "cookie-parser";
-// import { auth } from "../api/auth/middlewares";
+import { auth } from "../api/auth/middlewares";
 
 const app = express();
 app.use(cookieParser());
-//app.use(auth);
+app.use(auth);
 app.use('/api', router);
 
 export default app;
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,6 @@
 import app from "./core/api";
 import "./api/routes";
-const db = require('./utils/queries');
 
 app.listen(8080, () => {
 	console.log('Server started');
 })
-
-app.get('/users', db.getUsers);
-
-
-app.use('/auth', require('./api/auth/routes'));
