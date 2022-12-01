3201003439f00265b2c6bb20aeeaa374b8817e41
Nathan PERIER <periernathan2000@gmail.com>
2022-12-01 18:22:22 +0100

    Added queries for users

--- backend/src/database/queries/users.ts
+++ backend/src/database/queries/users.ts
@@ -1,64 +1,50 @@
 import { pool } from "../connect";
-import { Consumer } from 'src/types/functions';
+import { Callable, Consumer, ErrorHandler } from 'src/types/functions';
 import DatabaseUser from '../models/user';
+import { v4 as uuidv4 } from 'uuid';
+import { manageError } from "../errors";
 
 
-export function getUsers(request: any, response: any) {
-    pool.query('SELECT * FROM "User";', (error: any, results: any) => {
-        if (error) {
-            throw error;
-        }
-        response.status(200).json(results.rows);
-    });
+export function getUserById(user_id: string, consumer: Consumer<DatabaseUser | null>, onError?: ErrorHandler) {
+    pool.query('SELECT * FROM users WHERE id = $1;', [user_id]).then(qres => {
+        consumer(qres.rowCount == 1 ? qres.rows[0] : null);
+    }).catch(e => manageError(e, onError));
 }
 
-export function getUserById(request: any, response: any) {
-    const id = parseInt(request.params.id);
-
-    pool.query('SELECT * FROM users WHERE id = $1', [id], (error: any, results: any) => {
-        if (error) {
-            throw error;
-        }
-        response.status(200).json(results.rows);
-    });
+export function getUserByEmail(email: string, consumer: Consumer<DatabaseUser | null>, onError?: ErrorHandler) {
+    pool.query('SELECT * FROM users WHERE email = $1;', [email]).then(qres => {
+        consumer(qres.rowCount == 1 ? qres.rows[0] : null);
+    }).catch(e => manageError(e, onError));
 }
 
-export function createUser(request: any, response: any) {
-    const { name, email } = request.body;
+export function checkUserUniqueness(email: string, username: string, consumer: Consumer<boolean>, onError?: ErrorHandler) {
+    pool.query('SELECT COUNT(*) FROM users WHERE email = $1 OR username = $2;', [email, username]).then(qres => {
+        consumer(qres.rows[0]['count'] == 0);
+    }).catch(e => manageError(e, onError));
+}
 
+export function createUser(email: string, username: string, hash: string, salt: string, consumer: Consumer<string>, onError?: ErrorHandler) {
+    const uuid = uuidv4();
     pool.query(
-        'INSERT INTO users (name, email) VALUES ($1, $2) RETURNING *', [name, email],
-        (error: any, results: any) => {
-            if (error) {
-                throw error;
-            }
-            response.status(201).send(`User added with ID: ${results.rows[0].id}`);
-        }
-    );
+        'INSERT INTO users VALUES($1, $2, $3, $4, $5, \'PRIVATE\', \'USER\', false)', 
+        [uuid, email, username, hash, salt]
+    ).then(_ => {
+        consumer(uuid);
+    }).catch(e => manageError(e, onError));
 }
 
-export function updateUser(request: any, response: any) {
-    const id = parseInt(request.params.id);
-    const { name, email } = request.body;
-
+export function updateUserPassword(user_id: string, hash: string, salt: string, callback: Callable, onError?: ErrorHandler) {
     pool.query(
-        'UPDATE users SET name = $1, email = $2 WHERE id = $3', [name, email, id],
-        (error: any, results: any) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).send(`User modified with ID: ${id}`);
-        }
-    );
+        'UPDATE users SET password = $1, salt = $2 WHERE id = $3;', 
+        [hash, salt, user_id]
+    ).then(qres => {
+        console.log(qres);
+        callback();
+    }).catch(e => manageError(e, onError));
 }
 
-export function deleteUser(request: any, response: any) {
-    const id = parseInt(request.params.id);
-
-    pool.query('DELETE FROM users WHERE id = $1', [id], (error: any, results: any) => {
-        if (error) {
-            throw error;
-        }
-        response.status(200).send(`User deleted with ID: ${id}`);
-    });
+export function deleteUser(user_id: string, callback: Callable, onError?: ErrorHandler) {
+    pool.query('DELETE FROM users WHERE id = $1;', [user_id]).then(_ => {
+        callback();
+    }).catch(e => manageError(e, onError));
 }
