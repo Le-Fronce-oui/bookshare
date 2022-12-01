7f4ed89418a692115fb6745452dfd3d944672cf5
Nathan PERIER <periernathan2000@gmail.com>
2022-12-01 18:30:53 +0100

    Added database calls and remaining authentication routes

---
+++ backend/src/api/auth/hashing.ts
@@ -0,0 +1,11 @@
+import bcrypt from "bcrypt";
+
+export function newHash(cleartext: string): [string, string] {
+	const salt = bcrypt.genSaltSync(10);
+	const hash = bcrypt.hashSync(cleartext, salt);
+	return [hash, salt];
+}
+
+export function checkHash(secret: string, hash: string, salt: string): boolean {
+	return hash === bcrypt.hashSync(secret, salt);
+}
