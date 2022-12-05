a797ef83ebae761299536dbd15fc7ff1be7490ba
TristanDevin <tristan.devin@insa-lyon.fr>
2022-12-05 15:27:17 +0100

    update db

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,16 +1,41 @@
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
-
-async function run() {
-    
-}
-
-run()
-    .catch((e) => {
-        console.log(e);
-        process.exit(1);
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+async function main() {
+    const user1 = await prisma.user.upsert({
+        where: { email: 'alice@prisma.io' },
+        update: {},
+        create: {
+            email: 'alice@prisma.io',
+            username: 'Alice',
+            password: 'password',
+            salt: 'salt',
+            role: 'USER',
+            visibility : 'PUBLIC',
+        },
     })
-    .finally(async () => {
-        await prisma.$disconnect();
-    });
+   
+    const admin = await prisma.user.upsert({
+        where: { email: 'admin@prisma.io' },
+        update: {},
+        create: {
+            email: 'admin@prisma.io',
+            username: 'admin',
+            password: 'admin',
+            salt: 'salt',
+            role: 'ADMIN',
+            visibility : 'PRIVATE',
+        },
+    })
+
+    const 
+    console.log({ user1, admin })
+}
+main()
+    .then(async () => {
+        await prisma.$disconnect()
+    })
+    .catch(async (e) => {
+        console.error(e)
+        await prisma.$disconnect()
+        process.exit(1)
+    })
--- backend/src/api/organisations/routes.ts
+++ backend/src/api/organisations/routes.ts
@@ -1,9 +1,22 @@
 import router from "../../core/router";
-
+import { pool } from "../../database/connect";
 
 router.get('/org/:orgId/info', (req, res) => {
-	// TODO
+	req.params.orgId;
+	pool.query('SELECT * FROM organisations WHERE id = $1', [req.params.orgId], (err, result) => {
+		if(err) {
+			res.sendStatus(500);
+			return;
+		}
+		if(result.rowCount == 0) {
+			res.sendStatus(404);
+			return;
+		}
+		res.json(result.rows);
+	});
 	res.json('todo');
+
+	
 });
 
 router.get('/org/:orgId/prints', (req, res) => {
