bf2b0419da3c443260fe9a0378b69b35fb0dbe06
Nathan PERIER <periernathan2000@gmail.com>
2022-12-06 19:35:44 +0100

    Fixed book requests

--- backend/src/database/queries/books.ts
+++ backend/src/database/queries/books.ts
@@ -4,13 +4,13 @@ import { manageError } from "../errors";
 
 
 export function getBooks(consumer: Consumer<DatabaseBook[]>, onError?: ErrorHandler) {
-    pool.query('SELECT * FROM Books;').then(qres => {
+    pool.query('SELECT * FROM "Books";').then(qres => {
         consumer(qres.rows);
     }).catch(e => manageError(e, onError));
 }
 
 export function getBookById(book_id: string, consumer: Consumer<DatabaseBook | null>, onError?: ErrorHandler) {
-    pool.query('SELECT * FROM Books WHERE id = $1;', [book_id]).then(qres => {
+    pool.query('SELECT * FROM "Books" WHERE id = $1;', [book_id]).then(qres => {
         consumer(qres.rows.length > 0 ? qres.rows[0] : null);
     }).catch(e => manageError(e, onError));
 }
