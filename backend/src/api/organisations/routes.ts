4a2cf1f1e0627b124a3133bacb54429f9c9f94e3
TristanDevin <tristan.devin@insa-lyon.fr>
2022-12-08 15:19:13 +0100

    update org info

--- backend/src/api/organisations/routes.ts
+++ backend/src/api/organisations/routes.ts
@@ -1,23 +1,14 @@
 import router from "../../core/router";
 import { pool } from "../../database/connect";
 
-router.get('/org/:orgId/info', (req, res) => {
+router.get('/org/:orgId', (req, res) => {
 	req.params.orgId;
-	pool.query('SELECT * FROM organisations WHERE id = $1', [req.params.orgId], (err, result) => {
-		if(err) {
-			res.sendStatus(500);
-			return;
-		}
-		if(result.rowCount == 0) {
-			res.sendStatus(404);
-			return;
-		}
-		res.json(result.rows);
+	
+	res.json("todo")
 	});
-	res.json('todo');
 
 	
-});
+
 
 router.get('/org/:orgId/prints', (req, res) => {
 	// TODO
