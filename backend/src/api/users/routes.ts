24b9dd7bc0b70c37fa39a0332111cbdd68df9253
TristanDevin <tristan.devin@insa-lyon.fr>
2022-12-08 15:20:19 +0100

    update user/:userId/info

--- backend/src/api/users/routes.ts
+++ backend/src/api/users/routes.ts
@@ -1,5 +1,8 @@
+import { userInfo } from "os";
+import { getUserById } from "src/database/queries/users";
 import UserConnectedDTO from "src/dto/user_connected";
 import AuthenticatedUser from "src/types/internal/authenticated_user";
+import { DATABASE_USER } from "src/utils/env";
 import router from "../../core/router";
 import { authenticated } from "../auth/middlewares";
 
@@ -17,8 +20,24 @@ router.get('/user/connected', authenticated(401), (req, res) => {
 
 
 router.get('/user/:userId/info', (req, res) => {
-	// TODO
-	// Creation date ?
+	const id = req.params.userId
+	getUserById(id, db_user => {
+		if (db_user == null) {
+			res.sendStatus(404)
+			return
+		}
+		if (db_user.visibility != 'PUBLIC' && req.user === undefined) {
+			res.sendStatus(404)
+			return
+		} else {
+			let body: UserConnectedDTO = {
+				id: db_user.id,
+				username: db_user.username,
+				role: db_user.role,
+				organisations: []
+			}
+		}
+	})
 	res.json('todo');
 });
 
