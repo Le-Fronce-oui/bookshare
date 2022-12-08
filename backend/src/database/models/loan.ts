e1954d6dadf00852a9b202cbbd2bae5a9dbfe6c3
TristanDevin <tristan.devin@insa-lyon.fr>
2022-12-08 16:19:37 +0100

    loan db-model

---
+++ backend/src/database/models/loan.ts
@@ -0,0 +1,13 @@
+
+interface DatabaseLoan {
+   id: string,
+   organisation_id: string,
+    book_id: string,
+    borrower_id: string,
+    created_at: Date,
+    accepted_at: Date | null,
+    declined_at: Date | null,
+    borrowed_at: Date | null,
+    returned_at: Date | null
+
+}
